4843e5ae320aec6b005bad2c664ffe28191af60f
Volkya <matiasdylanc@gmail.com>
2020-01-08 17:52:08 -0300

    crud uno

--- src/routes/places.js
+++ src/routes/places.js
@@ -3,22 +3,52 @@ const router = express.Router();
 
 // traemos el model
 const Place = require('../models/place');
+// Place es un schema de la coleccion
 
 // router.get('/', (req, res) => {
 //     res.send('hello');
 // });
 
 // all places index
-
-
-router.get('/', (req, res) => {
-   res.render('places');
+router.get('/', async (req, res) => {
+    const places = await Place.find();
+   res.render('places', {
+       places
+   });
 });
 
-// add
+
+// add form and post
+router.get('/newPlace', (req, res) => {
+    res.render('newPlace');
+});
+
 router.post('/add', async (req, res) => {
-    const place = new Place(req.body);
-    await place.save();
+    const placeObject = new Place(req.body);
+    await placeObject.save();
+    console.log(placeObject);
+    res.redirect('/');
+});
+
+// edit form and post
+router.get('/edit/:id', async (req, res) => {
+    const { id } = req.params;
+    const place = await Place.findById(id);
+    res.render('editPlace', {
+        place
+    });
+});
+
+router.post('/edit/:id', async (req, res) => {
+    const { id } = req.params;
+    await Place.update({id: id}, req.body);
+    res.redirect('/');
+});
+
+// remove elements place by id
+router.get('/delete/:id', async (req, res) => {
+    const { id } = req.params;
+    await Place.remove({id: id});
     res.redirect('/');
 });
 
